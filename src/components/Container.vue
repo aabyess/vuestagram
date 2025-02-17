c9fcc8599b3339fc9f221027207c91ffb6ed3e41
abyss <a00411@naver.com>
2025-02-17 21:46:46 +0900

    0217
    인스타그램

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+    name : 'container',
+    components :{
+        Post,
+    },
+    props : {
+        Insta: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
