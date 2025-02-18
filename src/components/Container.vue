714c7ff853d4b000e56a614ea6ae12cf75409355
abyss <a00411@naver.com>
2025-02-18 20:05:53 +0900

    0218

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -8,11 +8,8 @@
     <div v-if="step == 1">
       <div class="upload-image" :style="`background-image:url(${이미지})`"></div>
       <div class="filters">
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
+        <FilterBox  :이미지="이미지" v-for="필터 in 필터들" :key="필터" :필터="필터"></FilterBox>
+        
       </div>
     </div>
     
@@ -29,11 +26,20 @@
 
 <script>
 import Post from "./Post.vue";
+import FilterBox from './FilterBox.vue'
 
 export default {
   name: "container",
+  data(){
+    return{
+      필터들 :[ "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson", 
+      "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua", 
+      "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"]
+    }
+  },
   components: {
     Post,
+    FilterBox,
   },
   props: {
     게시물: Array,
